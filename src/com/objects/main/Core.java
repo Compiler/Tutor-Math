7a962a85cf1a6cb58392ff0e872fba046f96f139
Luke <lr98lr@aol.com>
2016-10-30 22:06:55 -0400

    Objects

---
+++ src/com/objects/main/Core.java
@@ -0,0 +1,16 @@
+package com.objects.main;
+
+public class Core {
+
+	
+	public static void main(String [] args){
+		
+		Person obj = new Person("Emma");
+		Person obj2 = new Person("Luke");
+		obj.speakName();
+		obj2.speakName();
+		
+	}
+	
+	
+}
---
+++ src/com/objects/main/Person.java
@@ -0,0 +1,25 @@
+package com.objects.main;
+
+public class Person {
+	
+	
+	
+	private String myName;
+	
+	
+	public Person(String name){
+		
+		
+		this.myName = name;
+		
+	}
+	
+	
+	
+	public void speakName(){
+		
+		System.out.println("My name is " + myName);
+		
+	}
+
+}
