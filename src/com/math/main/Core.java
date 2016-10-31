49d7c75494e78fa8cb1962a38f7e45ebe079a21a
Luke <lr98lr@aol.com>
2016-10-30 21:35:30 -0400

    git instructions

--- src/com/math/main/Core.java
+++ src/com/math/main/Core.java
@@ -3,8 +3,19 @@ package com.math.main;
 public class Core {
 	
 	
+	/**
+	 * git add -A
+	 * 
+	 * git commit -m "your update message"
+	 * 
+	 * git push origin master
+	 * 
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args){
 		
+		System.out.println("Hello git");
 		
 	}
 
